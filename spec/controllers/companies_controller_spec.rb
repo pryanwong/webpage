d4e3edb57d06b820a5c79795a23a6403084b68b7
Sean Ferenci <sferenci1@yahoo.com>
2015-12-05 02:10:35 -0500

    RSPEC code for companies controller index method complete

--- spec/controllers/companies_controller_spec.rb
+++ spec/controllers/companies_controller_spec.rb
@@ -1,32 +1,48 @@
 require 'rails_helper'
 
-describe CompaniesController do
+describe CompaniesController, :type => :controller do
 
-    let(:company) { FactoryGirl.create(:company,:name,:license_rand, :with_divisions)}
-    let(:user_moderator) {FactoryGirl.create(:user, :moderator,:company_id => company.id)}
-    let(:user_user) {FactoryGirl.create(:user, :user,:company_id => company.id)}
+    let(:company1) { FactoryGirl.build(:company,:name,:license_rand, :with_divisions)}
+    let(:company2) { FactoryGirl.build(:company,:name,:license_rand, :with_divisions)}
+    let(:user) {double("user") }
+    #let(:user_admin) { FactoryGirl.create(:user, :admin,:company_id => company2.id) }
 
     describe "GET 'index'" do
+
+      it "renders the index page because user was logged in as admin" do
+         allow(user).to receive_messages(:id => 1, :admin? => true, :moderator? => false, :user? => false, :email => "sf@test.com", :company_id => 1, :company => company1)
+         allow(User).to receive(:where).and_return([user])
+         allow(User).to receive(:find).and_return(user)
+         allow(Company).to receive(:all).and_return([company1, company2])
+         allow(Company).to receive(:where).and_return([company1, company2])
+         login(user.email)
+         get :index
+         expect(response).to render_template("layouts/application")
+         expect(response).to render_template("index")
+         expect(assigns(:companies)).to match_array([company1, company2])
+      end
+
        it "renders the accessdenied page because no user was logged in" do
          get :index
          expect(response).to redirect_to("/accessdenied")
        end
-     end
 
-     describe "GET 'index'" do
        it "renders the index page because user was logged in as moderator" do
-          puts "User Email Is: #{user_moderator.email}"
-          login(user_moderator.email)
+          allow(user).to receive_messages(:id => 1, :admin? => false, :moderator? => true, :user? => false, :email => "sf@test.com", :company_id => 1, :company => company1)
+          allow(User).to receive(:where).and_return([user])
+          allow(User).to receive(:find).and_return(user)
+          login(user.email)
           get :index
           expect(response).to render_template("layouts/application")
           expect(response).to render_template("index")
+          expect(assigns(:companies)).to match_array([company1])
        end
-     end
 
-     describe "GET 'index'" do
        it "renders the accessdenied page because user was logged in as user" do
-          puts "User Email Is: #{user_user.email}"
-          login(user_user.email)
+          allow(user).to receive_messages(:id => 1, :admin? => false, :moderator? => false, :user? => true, :email => "sf@test.com", :company_id => 1, :company => company1)
+          allow(User).to receive(:where).and_return([user])
+          allow(User).to receive(:find).and_return(user)
+          login(user.email)
           get :index
           expect(response).to redirect_to("/accessdenied")
        end
--- spec/support/spec_test_helper.rb
+++ spec/support/spec_test_helper.rb
@@ -7,7 +7,16 @@ module SpecTestHelper
     request.session[:company_id] = user.company_id
   end
 
+  #def login(user_double)
+  #  request.session[:user_id] = user_double.id
+  #  request.session[:company_id] = user_double.company_id
+  #end
+
   def current_user
-    User.find(request.session[:user])
+    User.find(request.session[:user_id])
+  end
+
+  def current_ability
+    User.find(request.session[:user_id])
   end
 end
