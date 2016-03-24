3072765e42b9d07b906d21bf643b7f8ad7be553d
Sean Ferenci <sferenci1@yahoo.com>
2016-03-23 22:40:08 -0400

    Managing the case where there is no message to mail

--- app/mailers/message_image_mailer.rb
+++ app/mailers/message_image_mailer.rb
@@ -6,14 +6,21 @@ class MessageImageMailer < ActionMailer::Base
   def new_message(message)
     @message = message
     #email_list = [message.email1,message.email2,message.email3,message.email4]
-
-    drawing = Drawing.find(@message.drawing_id)
-    png = drawing.png
-    justpngdata = png.slice(png.index(",")+1..-1)
-    decodedImage = Base64.decode64(justpngdata)
-    attachments['image.png'] = decodedImage
-    emails = [@message.email1, @message.email2, @message.email3, @message.email4]
-    mail to: emails
+    if (Drawing.exists?(@message.drawing_id))
+       drawing = Drawing.find(@message.drawing_id);
+       if (drawing.png == "" || drawing.drawing == "")
+         attachments['image.svg'] = File.read("#{Rails.root}/app/assets/images/none/No_image_available.svg")
+       else
+         png = drawing.png
+         justpngdata = png.slice(png.index(",")+1..-1)
+         decodedImage = Base64.decode64(justpngdata)
+         attachments['image.png'] = decodedImage
+       end
+       emails = [@message.email1, @message.email2, @message.email3, @message.email4]
+       mail to: emails
+    else
+       flash[:error] = "Message Not Sent";
+    end
   end
 
 end
