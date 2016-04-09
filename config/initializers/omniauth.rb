9d086a72877a7ded7f5bc7e718d91b03290916ce
Sean Ferenci <sferenci1@yahoo.com>
2016-04-08 20:04:06 -0400

    Corrected google oauth2 scope

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,7 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, ENV["GOOGLE_CLIENT_ID"], ENV["GOOGLE_SECRET"],
     {
-      scope: ['userinfo.email, userinfo.profile']
+      scope: ['email','profile']
     }
 
   provider :linkedin, ENV["LINKEDIN_CLIENT_ID"], ENV["LINKEDIN_SECRET"],
