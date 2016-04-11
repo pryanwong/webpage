f1a171ead5e4dc90065319865aab92d853aa2847
Sean Ferenci <sferenci1@yahoo.com>
2016-04-11 19:15:34 -0400

    Updating application.scss so that fonts may be found #3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,9 +28,10 @@ Rails.application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
-
+  config.assets.paths << Rails.root.join("app", "assets", "fonts")
   # Generate digests for assets URLs.
   config.assets.digest = true
+
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
