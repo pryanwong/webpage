b3d4ffcd4e2e720de629629a3aadb5891c003bab
Sean Ferenci <sferenci1@yahoo.com>
2016-04-20 00:29:59 -0400

    Forgot to add png to assets

--- config/initializers/non_digest_assets.rb
+++ config/initializers/non_digest_assets.rb
@@ -1,3 +1,3 @@
 NonStupidDigestAssets.whitelist = [ /\.(?:svg|eot|woff|woff2|ttf)$/ ]
 NonStupidDigestAssets.whitelist += ["common/hs.png","common/hv.png","common/cross.gif","common/arrow.gif"]
-NonStupidDigestAssets.whitelist += [ /companies\/(GE|common|none)\/.*\.(?:jpg|jpeg|gif|svg)$/ ]
+NonStupidDigestAssets.whitelist += [ /companies\/(GE|common|none)\/.*\.(?:jpg|jpeg|gif|svg|png)$/ ]
