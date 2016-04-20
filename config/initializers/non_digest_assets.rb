6ac31e78243951fb1b9498f95cc3a79821a30d76
Sean Ferenci <sferenci1@yahoo.com>
2016-04-19 23:28:19 -0400

    Do not digest images

--- config/initializers/non_digest_assets.rb
+++ config/initializers/non_digest_assets.rb
@@ -1,2 +1,3 @@
 NonStupidDigestAssets.whitelist = [ /\.(?:svg|eot|woff|woff2|ttf)$/ ]
-NonStupidDigestAssets.whitelist += ["common/hs.png","common/hv.png","common/cross.gif","common/arrow.gif"]
+#NonStupidDigestAssets.whitelist += ["common/hs.png","common/hv.png","common/cross.gif","common/arrow.gif"]
+NonStupidDigestAssets.whitelist += ["GE/*","common/*","none/*"]
