6fa7f02ab4c13edcf222a189b7bf18f63bc843b6
Sean Ferenci <sferenci1@yahoo.com>
2016-04-24 17:17:54 -0400

    Added gif logo files

--- config/initializers/non_digest_assets.rb
+++ config/initializers/non_digest_assets.rb
@@ -1,3 +1,3 @@
 NonStupidDigestAssets.whitelist = [ /\.(?:svg|eot|woff|woff2|ttf)$/ ]
-NonStupidDigestAssets.whitelist += ["common/hs.png","common/hv.png","common/cross.gif","common/arrow.gif","Logo3_blue_front.gif"]
+NonStupidDigestAssets.whitelist += ["common/hs.png","common/hv.png","common/cross.gif","common/arrow.gif","Logo3_blue_front.gif","Logo3_blue_front_sm.gif"]
 NonStupidDigestAssets.whitelist += [ /companies\/.*\/.*\.(?:jpg|jpeg|gif|svg|png)$/ ]
