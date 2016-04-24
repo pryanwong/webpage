546448c2f7489d802f1c40bc5e7f9cdbe307dfe5
Sean Ferenci <sferenci1@yahoo.com>
2016-04-24 01:43:28 -0400

    changed client access type again try #3

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,8 +2,8 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, ENV["GOOGLE_CLIENT_ID"], ENV["GOOGLE_SECRET"],
     {
       scope: ['email','profile'],
-      :access_type => 'online',
-      :approval_prompt => 'force'
+      :access_type => :online,
+      :approval_prompt => :force
     }
 
   provider :linkedin, ENV["LINKEDIN_CLIENT_ID"], ENV["LINKEDIN_SECRET"],
