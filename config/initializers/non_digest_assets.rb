a8105cbaca5a013766efe769ce5ef042d702d8b4
Sean Ferenci <sferenci1@yahoo.com>
2016-04-24 23:50:21 -0400

    Updated image for google consent page

--- config/initializers/non_digest_assets.rb
+++ config/initializers/non_digest_assets.rb
@@ -1,3 +1,3 @@
 NonStupidDigestAssets.whitelist = [ /\.(?:svg|eot|woff|woff2|ttf)$/ ]
-NonStupidDigestAssets.whitelist += ["common/hs.png","common/hv.png","common/cross.gif","common/arrow.gif","Logo3_blue_front.gif","Logo3_blue_front_sm.gif", "Logo_no_text_sm.gif"]
+NonStupidDigestAssets.whitelist += ["common/hs.png","common/hv.png","common/cross.gif","common/arrow.gif","Logo3_blue_front.gif","Logo3_blue_front_sm.gif", "Logo_no_text_sm.gif", "Logo_no_text_google.png"]
 NonStupidDigestAssets.whitelist += [ /companies\/.*\/.*\.(?:jpg|jpeg|gif|svg|png)$/ ]
