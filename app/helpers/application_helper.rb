eaf7a5617900a97e1341018d4c007fe6510dc7dc
Sean Ferenci <sferenci1@yahoo.com>
2016-05-19 00:07:26 -0400

    Added Bootstrap layouts to flash messages, not all tested yet

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,18 @@ module ApplicationHelper
      link_to title, {:sort => column, :direction => direction, :srch_term => sort_search}, :class => css_class
   end
 
+  def bootstrap_class_for(flash_type)
+     case flash_type
+     when "success"
+       "alert-success"   # Green
+     when "error"
+       "alert-danger"    # Red
+     when "alert"
+       "alert-warning"   # Yellow
+     when "notice"
+       "alert-info"      # Blue
+     else
+       flash_type.to_s
+     end
+   end
 end
